85a94dddf3be5003e8192e2afc4fc3b2ae35592e
saichandra8995 <thammishettisaichandra@gmail.com>
2021-03-04 21:27:19 +0530

    Update Main.java

--- FRS/FRS/src/Main.java
+++ FRS/FRS/src/Main.java
@@ -3,20 +3,20 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-        Passenger passenger = new Passenger("4th Street", "Hyderabad","Telangana",
-                "nitin", "8790914565", "nk67@gmail.com");
+        Passenger passenger = new Passenger("H.no abccba", "Sircilla","Telangana",
+                "Saichandra", "9908082434", "thammishetti@gmail.com");
         Flight flight = new Flight("124Tr","Indigo", 186, 7);
 
-        RegularTicket regularTicket = new RegularTicket("56PNR234", "Hyd", "Del",
-                "20/12/20", "21/12/20", "12B",
-                2300, false, flight, passenger, "massage");
+        RegularTicket regularTicket = new RegularTicket("123235456", "Hyd", "Bglr",
+                "02/03/21", "03/03/21", "45",
+                1500, false, flight, passenger, "abc");
 
-        String hotelAddress = "Mariott Hotel, Delhi";
-        String[] locations = {"Agra","New Delhi"};
+        String hotelAddress = "Hotel, Bangalore";
+        String[] locations = {"kjhasd","asdf"};
 
-        TouristTicket touristTicket = new TouristTicket("56PNR354", "Hyd", "Del",
-                "20/12/20", "21/12/20", "12B",
-                2300, false, flight, passenger, hotelAddress,locations);
+        TouristTicket touristTicket = new TouristTicket("45234624", "Hyd", "Bglr",
+                "02/03/21", "03/03/21", "57",
+                1500, false, flight, passenger, hotelAddress,locations);
 
         printTicketDetails(regularTicket);
         printTicketDetails(touristTicket);
